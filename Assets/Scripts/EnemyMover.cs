e34bb0a3a42e733855f39cb6068a8cece5fde08c
F1ZZ <TheKubonn@hotmail.com>
2024-05-29 18:04:15 +0200

    Done two lessons
    - Introducing coroutines
    - Importing assets

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -1,21 +1,23 @@
-using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] private List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] private float waitTime = 1f;
 
     private void Start()
     {
-        PrintWaypointName();
+        StartCoroutine(FollowPath());
     }
 
-    void PrintWaypointName()
+    IEnumerator FollowPath()
     {
         foreach (Waypoint waypoint in path)
         {
-            Debug.Log(waypoint.name);
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(waitTime);
         }
     }
 }
