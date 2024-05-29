c8d84e64573875235e6d1c919567fc10336a0297
F1ZZ <TheKubonn@hotmail.com>
2024-05-29 17:36:28 +0200

    Done some of the course (more in description)
    Lessons Made:
    -Lists
    
    - Zrobiliśmy Enemy który stoi i będzie szukał drogi w która pójść, na jaki tile

---
+++ Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] private List<Waypoint> path = new List<Waypoint>();
+
+    private void Start()
+    {
+        PrintWaypointName();
+    }
+
+    void PrintWaypointName()
+    {
+        foreach (Waypoint waypoint in path)
+        {
+            Debug.Log(waypoint.name);
+        }
+    }
+}
